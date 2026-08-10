6b6759733a6ddc45beb3eace6d404a9ec8065348
sleepprof <gringrasp@googlemail.com>
2026-08-10 20:59:12 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="emotional-regulation-pocket-guide-v5";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const q=r.clone();caches.open(CACHE).then(x=>x.put(e.request,q));return r}).catch(()=>caches.match("./index.html"))))});
